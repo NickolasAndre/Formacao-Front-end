2f97a87893fa1bd9cb4a94ac75efa918ba437f60
Nickolas Andre <nickolas.sam3@gmail.com>
2024-01-02 10:36:51 -0300

    Special Numbers
    números especiais

--- JavaScript/2_OPERADORES/js/scripts.js
+++ JavaScript/2_OPERADORES/js/scripts.js
@@ -1,13 +1,20 @@
-// 1 - number
+// 1 - Number
 
 console.log(typeof 2);
 console.log(typeof 5.15);
 console.log(typeof -127);
 
-// 2 - op. aritméticas
+// 2 - Op. Aritméticas
 
 console.log( 2 + 4 );
 console.log( 10 - 3 );
 console.log( 2 * 4 );
 console.log( 6 / 2 );
-console.log( 4 + (2+6) );
+console.log( 4+(2+6));
+
+// 3 - Special Numbers
+
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(12 * "asd");
+console.log(typeof NaN);
