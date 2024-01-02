2d02db938e8b69cd1cfa34b5e63038f20ca3185a
Nickolas Andre <nickolas.sam3@gmail.com>
2024-01-02 10:17:03 -0300

    op. aritméticas
    Operadores

--- JavaScript/2_OPERADORES/js/scripts.js
+++ JavaScript/2_OPERADORES/js/scripts.js
@@ -3,3 +3,11 @@
 console.log(typeof 2);
 console.log(typeof 5.15);
 console.log(typeof -127);
+
+// 2 - op. aritméticas
+
+console.log( 2 + 4 );
+console.log( 10 - 3 );
+console.log( 2 * 4 );
+console.log( 6 / 2 );
+console.log( 4 + (2+6) );
