909bc06223852471fbe7dfa95bd4ee791c33f979
Nickolas Andre <nickolas.sam3@gmail.com>
2024-01-08 19:34:31 -0300

    variáveis

---
+++ JavaScript/3_ESTRUTURAS/scripts.js
@@ -0,0 +1,23 @@
+// 1 - variáveis
+
+let nome = "Nickolas";
+
+console.log(nome);
+
+nome = "Nickolas Andre";
+
+console.log(nome);
+
+const idade = 24;
+
+console.log(idade);
+
+console.log(typeof nome);
+
+console.log(typeof idade);
+
+// 2 - mais sobre variáveis
+
+let a = 10, b = 20, c = 30;
+
+console.log(a, b, c);
