18f3dd0dd42331ecd4d335d38c5129703eb1c2e6
Nickolas Andre <nickolas.sam3@gmail.com>
2024-01-14 11:18:04 -0300

    identação

--- JavaScript/3_ESTRUTURAS/scripts.js
+++ JavaScript/3_ESTRUTURAS/scripts.js
@@ -189,3 +189,15 @@ let q = 0;
 for(q; q <= 10; q = q + 1){
     console.log(`${q}`);
 }
+
+// 13- identação
+
+for(let u = 0; u < 10; u ++){
+    if(u * 2 > 10){
+        console.log(`Maior que 10! ${u}`)
+    } else{
+        if(u/2 === 0){
+        console.log("deu 0")
+     }
+    }
+}
