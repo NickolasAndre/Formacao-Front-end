e3d83c01f6b4ff8828a281facb7302d32ffee823
Nickolas Andre <nickolas.sam3@gmail.com>
2024-01-24 21:45:09 -0300

    escopo aninhado

--- JavaScript/4_FUNCOES/scripts.js
+++ JavaScript/4_FUNCOES/scripts.js
@@ -95,3 +95,25 @@ function testandoEscopo(){
 }
 testandoEscopo()
 console.log(`Y fora da função é: ${y}`);
+
+// 4 - escopo aninhado
+
+let m = 10;
+
+function escopoAninhado(){
+    let m = 20
+
+    if(true){
+        let m = 30
+
+        if(true){
+            let m = 40;
+
+            console.log(m);
+        }
+        console.log(m);
+    }
+    console.log(m);
+}
+escopoAninhado()
+console.log(m);
