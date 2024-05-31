63b2aec68505b1fdc3002d085dbdc4ab4bbb8c68
Nickolas Andre <nickolas.sam3@gmail.com>
2024-05-31 19:26:55 -0300

    arrays

---
+++ JavaScript/5_ARRAYS_E_OBJS/scripts.js
@@ -0,0 +1,14 @@
+// 1 - arrays
+
+const lista = [1, 2, 3, 4, 5];
+
+console.log(lista)
+console.log(typeof lista)
+
+const itens = ["nickolas", true, 1, 3.24, []];
+
+console.log(itens);
+
+const itens2 = [1, 4, 5, [6, 7, 8, 9], 10, 11];
+
+console.log(itens2)
