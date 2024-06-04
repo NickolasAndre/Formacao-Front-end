f611d37a78d6417a8b78672076ec330ef259fee1
Nickolas Andre <nickolas.sam3@gmail.com>
2024-06-03 21:54:38 -0300

    commit

--- JavaScript/5_ARRAYS_E_OBJS/scripts.js
+++ JavaScript/5_ARRAYS_E_OBJS/scripts.js
@@ -11,4 +11,23 @@ console.log(itens);
 
 const itens2 = [1, 4, 5, [6, 7, 8, 9], 10, 11];
 
-console.log(itens2)
+console.log(itens2);
+
+// Mais sobre Arrays
+
+const arr = ["a", "b", "c ", "d"]
+
+console.log(arr[3])
+
+
+// propriedades
+
+const number = [1, 2, 3, 4, 5, 6,];
+
+console.log(number.length)
+console.log(number["length"])
+console.log(number.pop)
+
+const myName = 'nico';
+
+console.log(myName.length)
