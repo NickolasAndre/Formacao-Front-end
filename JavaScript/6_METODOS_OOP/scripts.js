146e7e2d7472855ba60509ea0f66df5eef5e2ab7
Nickolas Andre <nickolas.sam3@gmail.com>
2024-08-26 21:24:53 -0300

    OOP métodos

---
+++ JavaScript/6_METODOS_OOP/scripts.js
@@ -0,0 +1,17 @@
+// 1 - Métodos
+
+const animal = {
+    nome: "bob",
+
+    nome2: "galinha",
+
+    latir: function(){
+        console.log("au au");
+    }
+};
+
+console.log(animal);
+
+console.log(animal.nome2);
+
+animal.latir();
