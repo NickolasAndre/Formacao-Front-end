afd35d6715dad50c608d7638b04c7ebcac7a608d
Nickolas Andre <nickolas.sam3@gmail.com>
2024-10-16 20:55:35 -0300

    debugger

--- JavaScript/7_DEBUG/script.js
+++ JavaScript/7_DEBUG/script.js
@@ -25,3 +25,26 @@ for( let i = 0; i < b; i++){
     a = a + 2
     console.log(a)
 }
+
+// 3 - debugger
+
+let c = 1;
+let d = 2;
+
+if(c == 1){
+    c = d + 2
+}
+
+//debugger;
+
+for(let i = 0; i < d; i++){
+    c = c + 2
+}
+
+console.log("Executou o loop");
+
+//debugger
+
+if (c > 25){
+    c = 25;
+}
