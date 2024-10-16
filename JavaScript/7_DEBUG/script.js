b1497c267d1c69478849e5976239ff1e2c24b593
Nickolas Andre <nickolas.sam3@gmail.com>
2024-10-16 20:30:30 -0300

    console.log

--- JavaScript/7_DEBUG/script.js
+++ JavaScript/7_DEBUG/script.js
@@ -8,4 +8,20 @@ const opa = "teste"
 
 console.log(opa);
 
-delete [].length; 
+//delete [].length; 
+
+// 2 - console.log
+
+let a = 1;
+let b = 2;
+
+if(a == 1 ){
+    a = b + 2
+}
+
+console.log(a)
+
+for( let i = 0; i < b; i++){
+    a = a + 2
+    console.log(a)
+}
