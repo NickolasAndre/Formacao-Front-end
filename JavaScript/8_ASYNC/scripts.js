53f548fa24a55d5c8cd5aa62d57fc745a4955ae6
Nickolas Andre <nickolas.sam3@gmail.com>
2024-10-18 22:04:40 -0300

    promises

--- JavaScript/8_ASYNC/scripts.js
+++ JavaScript/8_ASYNC/scripts.js
@@ -7,3 +7,33 @@ setTimeout(function(){
 }, 2000)
 
 console.log("Ainda não executou 2");
+
+// 2 - setInterval
+
+console.log("Ainda não começou");
+
+/*
+
+setInterval(function(){
+    console.log("Intervalo assícrono")
+}, 3000)
+
+*/
+
+console.log("Ainda não começou 2");
+
+//3 - Promise
+
+const promessa = Promise.resolve(5 + 5);
+
+console.log("Algum código");
+
+promessa.then((value) =>{
+    console.log(`A soma é ${value}`)
+    return value;
+})
+
+.then((value) => value - 1)
+.then((value) => console.log(`Agora é ${value}`))
+
+console.log("Outro código");
