59dc8bc71e28f90947755ed805d8596e46f5b877
Nickolas Andre <nickolas.sam3@gmail.com>
2024-10-22 20:43:40 -0300

    falha no promise

--- JavaScript/8_ASYNC/scripts.js
+++ JavaScript/8_ASYNC/scripts.js
@@ -37,3 +37,14 @@ promessa.then((value) =>{
 .then((value) => console.log(`Agora é ${value}`))
 
 console.log("Outro código");
+
+// 4 - falha na promises
+
+Promise.resolve(4 * "asd")
+.then((n) => {
+    if(Number.isNaN(n)){
+        throw new Error("Valores inválidos")
+    }
+})
+
+.catch((err) => console.log(`Um erro ocorreu: ${err}`))
