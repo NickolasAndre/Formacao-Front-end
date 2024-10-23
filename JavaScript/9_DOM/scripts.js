a2fe81e17a092fb8abefa0bd55ed4bbebbf72fd7
Nickolas Andre <nickolas.sam3@gmail.com>
2024-10-23 18:11:14 -0300

    movendo o DOM

---
+++ JavaScript/9_DOM/scripts.js
@@ -0,0 +1,9 @@
+// 1 - movendo-se pelo DOM
+
+console.log(document.body);
+
+console.log(document.body.childNodes[1])
+
+console.log(document.body.childNodes[1].childNodes)
+
+console.log(document.body.childNodes[1].childNodes[1].textContent)
