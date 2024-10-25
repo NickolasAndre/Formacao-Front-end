8c282c66945454118edfd98c55413dc226019ff8
Nickolas Andre <nickolas.sam3@gmail.com>
2024-10-25 15:48:23 -0300

    removendo eventos

--- JavaScript/10_EVENTOS/scripts.js
+++ JavaScript/10_EVENTOS/scripts.js
@@ -5,3 +5,35 @@ const btn = document.querySelector("#my-button");
 btn.addEventListener("click", function(){
     console.log("Clicou aqui!")
 })
+
+// 2 - removendo eventos
+
+const segundoBtn = document.querySelector("#btn");
+
+function imprimirMensagem(){
+    console.log("Teste");
+}
+
+segundoBtn.addEventListener("click", imprimirMensagem);
+
+const terBtn = document.querySelector("#other-btn");
+
+terBtn.addEventListener("click", () =>{
+    console.log("Evento removido")
+    segundoBtn.removeEventListener("click", imprimirMensagem)
+})
+
+const att = document.querySelector("#att");
+
+function attimprimir(){
+    console.log("testeeeee");
+}
+
+att.addEventListener("click", attimprimir);
+
+const ate = document.querySelector("#ate");
+
+ate.addEventListener("click", () =>{
+    console.log("removeu");
+    att.removeEventListener("click", attimprimir)
+})
