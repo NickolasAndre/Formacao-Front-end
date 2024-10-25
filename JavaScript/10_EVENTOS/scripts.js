63c4ad72c0c5d440235a31abe71cff8b7a21c1eb
Nickolas Andre <nickolas.sam3@gmail.com>
2024-10-25 16:46:43 -0300

    Propagação

--- JavaScript/10_EVENTOS/scripts.js
+++ JavaScript/10_EVENTOS/scripts.js
@@ -47,3 +47,18 @@ myTitle.addEventListener("click", (event) =>{
     console.log(event.target)
     console.log(event.pointerType)
 })
+
+// 4 - Propagação
+
+const containerBtn = document.querySelector("#btn-container");
+
+const btnInsideContainer = document.querySelector("#div-btn");
+
+containerBtn.addEventListener("click", () =>{
+    console.log("Evento 1")
+})
+
+btnInsideContainer.addEventListener("click", (e) =>{
+    e.stopPropagation()
+    console.log("evento 2")
+})
