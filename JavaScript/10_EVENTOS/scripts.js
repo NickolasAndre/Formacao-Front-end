a092938b2b8181eba2fee865b433ad717d4f0186
Nickolas Andre <nickolas.sam3@gmail.com>
2024-10-26 17:33:47 -0300

    debounce

--- JavaScript/10_EVENTOS/scripts.js
+++ JavaScript/10_EVENTOS/scripts.js
@@ -137,3 +137,25 @@ window.addEventListener("beforeunload", (e) =>{
     e.preventDefault()
    
 })
+
+// 12 - debounce
+
+const debounce = (f, delay) => {
+    let timeout;
+
+    return(...arguments) =>{
+        if(timeout){
+            clearTimeout(timeout)
+        }
+
+        timeout = setTimeout(() =>{
+            f.apply(arguments);
+        }, delay)
+    }
+
+}
+
+window.addEventListener("mousemove", debounce(() => {
+        console.log("Executando a cada 400ms")
+    }, 400)
+)
